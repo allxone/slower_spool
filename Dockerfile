17d06b6daeb68757942feb49e77b3b8e91b5dc08
allxone <allxone@hotmail.com>
2015-04-10 17:51:54 +0000

    running

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ COPY slowerSpool.sh /tmp/slowerSpool.sh
 RUN mkdir /tmp/input && mkdir /tmp/output && chmod u+x /tmp/slowerSpool.sh
 VOLUME /tmp/input
 VOLUME /tmp/output
-CMD /tmp/slowerSpool.sh 2000 1 trip_data_10.csv trip_data_10.csv
-ENTRYPOINT /bin/sh
+CMD /tmp/slowerSpool.sh 10000 0.5 trip_data_12.csv trip_data_12.csv
--- slowerSpool.sh
+++ slowerSpool.sh
@@ -10,24 +10,25 @@ echo $OUTPUT
 
 c=0
 l=0
+f=100000
 
 CHUNK=$1
 SLEEP=$2
 INPUT=/tmp/input/$3
-OUTPUT=/tmp/output/$4
 
 test ! -f "$INPUT" && help && exit 1
-test  -f "$OUTPUT" && echo "please remove $OUTPUT" && exit 1
 
 cat $INPUT | while read a
 do
 	c=$(($c+1))
 	l=$(($l+1))
-	echo $a >>$OUTPUT
+	echo $a >>/tmp/output/hide_$4$f
 	test $l != $CHUNK && continue
+	mv /tmp/output/hide_$4$f /tmp/output/$4$f
 	sleep $SLEEP
 	echo -ne "\r$c"
 	l=0
+	f=$(($f+1))
 done
 date
 
